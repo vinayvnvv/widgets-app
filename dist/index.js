18306074bf99841028ccce0201dc4d22362ae201
Vinay B V <vinaybv@ALIPL1624.local>
2022-07-07 18:58:34 +0530

    dist;

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+var Button = function Button(_ref) {
+  var children = _ref.children;
+  return /*#__PURE__*/React.createElement("button", null, children);
+};
+
+exports.Button = Button;
+//# sourceMappingURL=index.js.map
---
+++ dist/index.modern.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Button = ({
+  children
+}) => {
+  return /*#__PURE__*/React.createElement("button", null, children);
+};
+
+export { Button };
+//# sourceMappingURL=index.modern.js.map
